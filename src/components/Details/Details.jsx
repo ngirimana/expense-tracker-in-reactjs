bd2d99b1574c472de545477d14b79c3eb1566063
ngirimana <chadrackngirimana@gmail.com>
2022-02-10 17:50:56 +0200

    dispalying data on the graph

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
-import { Drought } from "react-chartjs-2";
+import { Doughnut} from "react-chartjs-2";
 import useStyles from "./styles";
+import useTransactions from "../../useTransactions";
+
 
 function Details({ title }) {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
   return (
     <Card className={title === "Income" ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/* <Drought data="Data"/> */}
+        <Typography variant="h5">${total}</Typography>
+        {chartData && <Doughnut data={chartData} />}
       </CardContent>
     </Card>
   );
