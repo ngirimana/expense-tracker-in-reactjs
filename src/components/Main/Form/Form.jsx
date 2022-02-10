265549be0c5dd281376380ac30cdde184cabb7a1
ngirimana <chadrackngirimana@gmail.com>
2022-02-10 16:26:14 +0200

    add and delete transaction

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useContext} from "react";
 import {
   TextField,
   Typography,
@@ -10,9 +10,24 @@ import {
   MenuItem,
 } from "@material-ui/core";
 import useStyles from "./styles";
+import { ExpenseTrackerContext } from "../../../context/context";
+import { v4 as uuid } from 'uuid';
+const initialState = {
+  amount: '',
+  category: '',
+  type: '',
+  date:new Date(),
+}
 
 function Form() {
   const classes = useStyles();
+  const [formData, setFormData] = useState(initialState);
+  const { addTransaction } = useContext(ExpenseTrackerContext);
+  const createTransaction = () => {
+    const transaction = { ...formData, amount: Number(formData.amount), id: uuid() };
+    addTransaction(transaction);
+    setFormData(initialState);
+  }
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -23,7 +38,10 @@ function Form() {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select
+            value={formData.type}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
             <MenuItem value="Income">Income</MenuItem>
             <MenuItem value="Expense">Expense</MenuItem>
           </Select>
@@ -32,19 +50,42 @@ function Form() {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <MenuItem value="business">Business</MenuItem>
             <MenuItem value="salary">salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type="number"
+          label="Amount"
+          fullWidth
+          value={formData.amount}
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth />
+        <TextField
+          type="date"
+          label="Date"
+          fullWidth
+          value={formData.date}
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
       </Grid>
-      <Button className={classes.button} variant="outlined" color="primary" fullWidth>
+      <Button
+        className={classes.button}
+        variant="outlined"
+        color="primary"
+        fullWidth
+        onClick={createTransaction}
+      >
         Create
       </Button>
     </Grid>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from './context/context';
 import App from './App';
-import './index.css'
-ReactDom.render(<App/>,document.getElementById('root'))
+
+import './index.css';
+ReactDom.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
